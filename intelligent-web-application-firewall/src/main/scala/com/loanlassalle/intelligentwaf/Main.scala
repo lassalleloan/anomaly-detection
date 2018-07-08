58dd82f1cb5bcf61ef5bde9c1ca8c092a5aa209f
Loan Lassalle <18124884+lassalleloan@users.noreply.github.com>
2018-07-08 10:59:06 +0200

    Data preparation basic statistic

--- intelligent-web-application-firewall/src/main/scala/com/loanlassalle/intelligentwaf/Main.scala
+++ intelligent-web-application-firewall/src/main/scala/com/loanlassalle/intelligentwaf/Main.scala
@@ -8,16 +8,28 @@ object Main {
   def main(args: Array[String]): Unit = {
     Logger.getLogger("org").setLevel(Level.ERROR)
 
+    val filename = "/csic_2010_http_dataset/normalTrafficTraining-10-requests.txt"
+
     val spark = SparkSession.builder
       .master("local[*]")
       .appName("Spark Intelligent WAF")
       .getOrCreate()
     val sc = spark.sparkContext
-    val filename = "/csic_2010_http_dataset/normalTrafficTraining-10-requests.txt"
+
     val rawHttpRequests = RawHttpRequest.parseRawRequestFile(getClass.getResource(filename).getPath)
 
-    println("num lines: " + sc.textFile(getClass.getResource(filename).getPath).count())
-    rawHttpRequests.foreach(println)
+    println("Basic statistics")
+    println("Number of HTTP request: " + rawHttpRequests.size)
+    println("Number of unique path : " + rawHttpRequests.map(_.path).distinct.size)
+    println("List of unique path : " + rawHttpRequests.map(_.path).distinct)
+    println("Number of unique parameter : " + rawHttpRequests.flatMap(_.parameters.keySet).distinct.size)
+    println("List of unique parameter : " + rawHttpRequests.flatMap(_.parameters.keySet).distinct)
+    println("Number of unique header : " + rawHttpRequests.flatMap(_.requestHeaders.keySet).distinct.size)
+    println("List of unique header : " + rawHttpRequests.flatMap(_.requestHeaders.keySet).distinct)
+    println()
+    println("Dataset")
+
+    println(rawHttpRequests.head.fileExtension)
 
     sc.stop()
     spark.stop()
--- intelligent-web-application-firewall/src/main/scala/com/loanlassalle/intelligentwaf/RawHttpRequest.scala
+++ intelligent-web-application-firewall/src/main/scala/com/loanlassalle/intelligentwaf/RawHttpRequest.scala
@@ -9,37 +9,133 @@ class RawHttpRequest(val requestLine: String,
                      val requestHeaders: Map[String, String],
                      val messageBody: String) {
 
-  private val requestLineSplited = requestLine.split(" ")
-  private val method = requestLineSplited.head
-  private val url = new URL(requestLineSplited.tail.head)
-  private val path = url.getPath
-  private val query = if (url.getQuery == null) "" else url.getQuery
-  private val standard = requestLineSplited.last
+  private val requestLineSplit: Array[String] = requestLine.split(" ")
 
-  def segmentCount: Long = path.count(_ == '/')
-  def alphaNumRatio: Double = path.count(c => c.isLetterOrDigit || c == '/').toDouble / path.length
-  def parameterCount: Long = query.count(_ == '=')
-  def isUserAgentPresent: Boolean = requestHeaders.isDefinedAt("User-Agent")
-  def isPersistentConnection: Boolean = requestHeaders.getOrElse("Connection", "") == "keep-alive"
+  val method: String = requestLineSplit.head
+  val url: URL = new URL(requestLineSplit.tail.head)
+  val path: String = if (url.getPath == null) "" else url.getPath
+  val query: String = if (url.getQuery == null) "" else url.getQuery
+  val parameters: Map[String, List[String]] = parseQuery
+  val standard: String = requestLineSplit.last
 
   override def toString: String =
-    s"$method," +
-      s"$path,$segmentCount,${path.length},${"%.2f".format(alphaNumRatio)}," +
-      s"$query,${query.length},$parameterCount,$standard," +
-      s"${requestHeaders.mkString(",")},${requestHeaders.size},$isUserAgentPresent,$isPersistentConnection," +
-      s"$messageBody,${messageBody.length}"
+    s"$requestLine\n" +
+      s"${requestHeaders.mkString("\n")}\n" +
+      s"\n${if (messageBody.length > 0) messageBody else ""}"
+
+  def segmentCount: Int = path.count(_ == '/')
+
+  def isFile: Int = path.substring(path.lastIndexOf('/') + 1).contains('.').compareTo(false)
+
+  def fileExtension: String = path.substring(path.lastIndexOf('.') + 1)
+
+  def parameterCount: Int = query.count(_ == '=')
+
+  def isStandardHeader(header: String): Int = RawHttpRequest.StandardHeaders.contains(header).compareTo(false)
+
+  def isHeaderPresent(header: String): Int = requestHeaders.contains(header).compareTo(false)
+
+  def isPersistentConnection: Int = (requestHeaders.getOrElse("Connection", "") == "keep-alive").compareTo(false)
+
+  def nonStandardHeaderRatio: Double =
+    if (requestHeaders.isEmpty)
+      0
+    else
+      1 - standardHeaderRatio
+
+  def standardHeaderRatio: Double =
+    if (requestHeaders.isEmpty)
+      0
+    else
+      requestHeaders.size / RawHttpRequest.StandardHeaders.size.toDouble
+
+  def isBody: Int = messageBody.nonEmpty.compare(false)
+
+  def lineCount(str: String): Int = str.split("\r\n|\r|\n").length
+
+  def wordCount(str: String): Int = str.split("\\w+").length
+
+  private def nonPrintableCharRatio(str: String): Double =
+    if (str.isEmpty)
+      str.length
+    else
+      1 - printableCharRatio(str)
+
+  private def symbolRatio(str: String): Double =
+    if (str.isEmpty)
+      str.length
+    else
+      (printableCharRatio(str) - letterRatio(str) - digitRatio(str)) * str.length
+
+  private def printableCharRatio(str: String): Double =
+    if (str.isEmpty)
+      str.length
+    else
+      "[ -~]".r.findAllIn(str).length / str.length.toDouble
+
+  private def letterRatio(str: String): Double =
+    if (str.isEmpty)
+      str.length
+    else
+      str.count(_.isLetter) / str.length.toDouble
+
+  private def digitRatio(str: String): Double =
+    if (str.isEmpty)
+      str.length
+    else
+      str.count(_.isDigit) / str.length.toDouble
+
+  private def parseQuery: Map[String, List[String]] = {
+    val separator = '&'
+
+    if (query.isEmpty)
+      Map[String, List[String]]()
+    else
+      query.split(separator)
+        .map(parseParameter)
+        .groupBy(_._1)
+        .map(t => t._1 -> t._2.map(_._2).toList)
+  }
+
+  private def parseParameter(parameter: String): (String, String) = {
+    val separator = '='
+
+    val index = parameter.indexOf(separator)
+    val key = if (index > 0) parameter.substring(0, index) else parameter
+    val value = if (index > 0 && parameter.length > index + 1) parameter.substring(index + 1) else ""
+
+    key -> value
+  }
 
 }
 
 object RawHttpRequest {
 
-  def parseHeader(header: String): (String, String) = {
-    val separator = ": "
-    val index = header.indexOf(separator)
-    if (index == -1)
-      throw new NoSuchElementException("Invalid Header Parameter: " + header)
+  val StandardHeaders = List("Accept", "Accept-Charset", "Accept-Datetime", "Accept-Encoding",
+    "Accept-Language", "Authorization", "Cache-Control", "Connection",
+    "Content-Length", "Content-MD5", "Content-Type", "Cookie",
+    "Date", "Expect", "From", "Host",
+    "If-Match", "If-Modified-Since", "If-None-Match", "If-Range",
+    "If-Unmodified-Since", "Max-Forwards", "Origin", "Pragma",
+    "Proxy-Authorization", "Range", "Referer", "TE",
+    "User-Agent", "Upgrade", "Via", "Warning")
 
-    (header.substring(0, index), header.substring(index + separator.length, header.length()))
+  /**
+    * Parse raw HTTP requests contain in file
+    *
+    * @param filename name of file contains raw HTTP requests
+    * @throws java.io.FileNotFoundException if an I/O error occurs reading the input stream
+    * @throws NoSuchElementException        if HTTP Request is malformed
+    */
+  def parseRawRequestFile(filename: String): ListBuffer[RawHttpRequest] = {
+    val iterator = Source.fromFile(filename).getLines()
+    val httpRequests = ListBuffer[RawHttpRequest]()
+
+    while (iterator.hasNext) {
+      httpRequests += parseRawRequest(iterator)
+    }
+
+    httpRequests
   }
 
   /**
@@ -65,22 +161,14 @@ object RawHttpRequest {
     new RawHttpRequest(requestLine, requestHeaders, messageBody)
   }
 
-  /**
-    * Parse raw HTTP requests contain in file
-    *
-    * @param filename name of file contains raw HTTP requests
-    * @throws java.io.FileNotFoundException if an I/O error occurs reading the input stream
-    * @throws NoSuchElementException if HTTP Request is malformed
-    */
-  def parseRawRequestFile(filename: String): ListBuffer[RawHttpRequest] = {
-    val iterator = Source.fromFile(filename).getLines()
-    val httpRequests = ListBuffer[RawHttpRequest]()
+  private def parseHeader(header: String): (String, String) = {
+    val separator = ": "
 
-    while (iterator.hasNext) {
-      httpRequests += parseRawRequest(iterator)
-    }
+    val index = header.indexOf(separator)
+    if (index == -1)
+      throw new NoSuchElementException("Invalid Header Parameter: " + header)
 
-    httpRequests
+    header.substring(0, index) -> header.substring(index + separator.length, header.length())
   }
 
 }
