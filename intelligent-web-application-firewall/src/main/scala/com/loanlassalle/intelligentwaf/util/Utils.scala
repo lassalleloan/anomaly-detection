1a571ddb106f5f7c962b31acccd5edb674c98c9e
Loan Lassalle <18124884+lassalleloan@users.noreply.github.com>
2018-07-18 10:32:54 +0200

    Comments toolbox

--- intelligent-web-application-firewall/src/main/scala/com/loanlassalle/intelligentwaf/util/Utils.scala
+++ intelligent-web-application-firewall/src/main/scala/com/loanlassalle/intelligentwaf/util/Utils.scala
@@ -1,7 +1,16 @@
 package com.loanlassalle.intelligentwaf.util
 
+/**
+  * A toolbox for basics statistic
+  */
 object Utils {
 
+  /**
+    * Gets total non printable characters ratio of sequence of string
+    *
+    * @param seq sequence of string
+    * @return total non printable characters ratio of sequence of string
+    */
   def nonPrintableCharRatio(seq: Seq[String]): Double =
     if (seq.isEmpty || seq.forall(_.isEmpty))
       0
@@ -9,18 +18,34 @@ object Utils {
       seq.foldLeft(0)((sum, value) => sum + nonPrintableCharCount(value)) /
         seq.foldLeft(0.0)((sum, value) => sum + value.length)
 
+  /**
+    * Counts number of non printable characters in a string
+    * @param str string
+    * @return number of non printable characters in a string
+    */
   def nonPrintableCharCount(str: String): Int =
     if (str.isEmpty)
       str.length
     else
       str.length - printableCharCount(str)
 
+  /**
+    * Counts number of printable characters in a string
+    * @param str string
+    * @return number of printable characters in a string
+    */
   def printableCharCount(str: String): Int =
     if (str.isEmpty)
       str.length
     else
       "[ -~]".r.findAllIn(str).length
 
+  /**
+    * Gets total printable characters ratio of sequence of string
+    *
+    * @param seq sequence of string
+    * @return total printable characters ratio of sequence of string
+    */
   def printableCharRatio(seq: Seq[String]): Double =
     if (seq.isEmpty || seq.forall(_.isEmpty))
       0
@@ -28,6 +53,12 @@ object Utils {
       seq.foldLeft(0)((sum, value) => sum + printableCharCount(value)) /
         seq.foldLeft(0.0)((sum, value) => sum + value.length)
 
+  /**
+    * Gets total symbols ratio of sequence of string
+    *
+    * @param seq sequence of string
+    * @return total symbols ratio of sequence of string
+    */
   def symbolRatio(seq: Seq[String]): Double =
     if (seq.isEmpty || seq.forall(_.isEmpty))
       0
@@ -35,12 +66,23 @@ object Utils {
       seq.foldLeft(0)((sum, value) => sum + symbolCount(value)) /
         seq.foldLeft(0.0)((sum, value) => sum + value.length)
 
+  /**
+    * Counts number of symbols in a string
+    * @param str string
+    * @return number of symbols in a string
+    */
   def symbolCount(str: String): Int =
     if (str.isEmpty)
       str.length
     else
       printableCharCount(str) - letterCount(str) - digitCount(str)
 
+  /**
+    * Gets total letters ratio of sequence of string
+    *
+    * @param seq sequence of string
+    * @return total letters ratio of sequence of string
+    */
   def letterRatio(seq: Seq[String]): Double =
     if (seq.isEmpty || seq.forall(_.isEmpty))
       0
@@ -48,12 +90,23 @@ object Utils {
       seq.foldLeft(0)((sum, value) => sum + letterCount(value)) /
         seq.foldLeft(0.0)((sum, value) => sum + value.length)
 
+  /**
+    * Counts number of letters in a string
+    * @param str string
+    * @return number of letters in a string
+    */
   def letterCount(str: String): Int =
     if (str.isEmpty)
       str.length
     else
       str.count(_.isLetter)
 
+  /**
+    * Gets total digits ratio of sequence of string
+    *
+    * @param seq sequence of string
+    * @return total digits ratio of sequence of string
+    */
   def digitRatio(seq: Seq[String]): Double =
     if (seq.isEmpty || seq.forall(_.isEmpty))
       0
@@ -61,10 +114,14 @@ object Utils {
       seq.foldLeft(0)((sum, value) => sum + digitCount(value)) /
         seq.foldLeft(0.0)((sum, value) => sum + value.length)
 
+  /**
+    * Counts number of digits in a string
+    * @param str string
+    * @return number of digits in a string
+    */
   def digitCount(str: String): Int =
     if (str.isEmpty)
       str.length
     else
       str.count(_.isDigit)
-
 }
